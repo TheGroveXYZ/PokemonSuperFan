71451d6f7ebb57881b6b388f5d2ee634389d101c
TheGroveXYZ <windgrovedanieljr@gmail.com>
2025-12-12 11:16:27 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// use variables to make your code shorter
+var j1;
+var j2;
+var j3;
+var j4;
+
+// declare your functions
+function ninja() {
+  document.getElementById("greninjaSound").play();
+  document.getElementById("myVideo").src =
+    "https://www.youtube.com/embed/5UuFlwqDyD0?si=wv4J49Kksmvw1bg_";
+}
+
+function sceptile() {
+  document.getElementById("sceptileSound").play();
+  document.getElementById("myVideo").src =
+    "https://www.youtube.com/embed/fAyrEjvhnEc?si=bH6SSD7Z3gpF1t-5";
+}
+function playSound() {
+  document.getElementById("greninjaSound").play();
+  document.getElementById("sceptileSound").play();
+}
